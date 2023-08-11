ff35f44a3204ebf9ebdc642ab5db3eeda7f75eca
Tamilselvan S <tamilselvan.s@ctlep.local>
2023-08-11 12:39:14 +0530

    removed comments

--- src/Services/TestingForAxios.js
+++ src/Services/TestingForAxios.js
@@ -1,14 +1,6 @@
 
-// import axios from 'axios';
 import { React/*useState*/ } from 'react';
-
-// const STS_URL = ""
-
 function GetSimpleData(){
-    // const[ get, setGet] =useState();
-    // const[ error, setError] =useState();
-
-    // axios.get(STS_URL).then(res=>{setGet(res.data)}).catch(err=>{setError(err)})
     return (
         <h3>hello</h3>
     );
